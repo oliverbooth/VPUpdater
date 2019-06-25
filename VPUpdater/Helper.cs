6555a4bf7f7419f0909f82a124403ed522c23f96
Oliver Booth <me@olivr.me>
2019-06-25 13:28:25 +0100

    ✨ Add useful types

---
+++ VPUpdater/Helper.cs
@@ -0,0 +1,37 @@
+﻿#region Copyright
+
+// -----------------------------------------------------------------------
+//  <copyright file="Helper.cs" company="VPUpdater">
+//      (C) 2019 Oliver Booth. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+#endregion
+
+namespace VPUpdater
+{
+    #region Using Directives
+
+    using System;
+
+    #endregion
+
+    /// <summary>
+    /// A set of helper methods.
+    /// </summary>
+    /// <remarks>I understand that "helper" classes are bad practice, but this isn't exactly a
+    /// commercial-grade app.</remarks>
+    public static class Helper
+    {
+        #region Methods
+
+        /// <summary>
+        /// Gets the machine's architecture.
+        /// </summary>
+        /// <returns>Returns a <see cref="MachineArch"/> representing the machine architecture.</returns>
+        public static MachineArch GetMachineArch() =>
+            Environment.Is64BitOperatingSystem ? MachineArch.x64 : MachineArch.x86;
+
+        #endregion
+    }
+}
---
+++ VPUpdater/MachineArch.cs
@@ -0,0 +1,38 @@
+﻿#region Copyright
+
+// -----------------------------------------------------------------------
+//  <copyright file="MachineArch.cs" company="VPUpdater">
+//      (C) 2019 Oliver Booth. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+#endregion
+
+namespace VPUpdater
+{
+    #region Using Directives
+
+    using System.ComponentModel;
+    using System.Diagnostics.CodeAnalysis;
+
+    #endregion
+
+    /// <summary>
+    /// An enumeration of compatible machine architectures.
+    /// </summary>
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public enum MachineArch
+    {
+        /// <summary>
+        /// x86 instruction set.
+        /// </summary>
+        [Description("x86 instruction set.")]
+        x86 = 0,
+
+        /// <summary>
+        /// x86-64 instruction set.
+        /// </summary>
+        [Description("x64 instruction set.")]
+        x64 = 1
+    }
+}
