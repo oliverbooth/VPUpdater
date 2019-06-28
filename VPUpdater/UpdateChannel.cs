db22a466eaf5a8d39bc6aa9286b0d9b8de3fc2b4
Oliver Booth <me@olivr.me>
2019-06-28 16:31:45 +0100

    ➕ Add UpdateChannel enum

---
+++ VPUpdater/UpdateChannel.cs
@@ -0,0 +1,36 @@
+﻿#region Copyright
+
+// -----------------------------------------------------------------------
+//  <copyright file="UpdateChannel.cs" company="VPUpdater">
+//      (C) 2019 Oliver Booth. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+#endregion
+
+namespace VPUpdater
+{
+    #region Using Directives
+
+    using System.ComponentModel;
+
+    #endregion
+
+    /// <summary>
+    /// An enumeration of update channels.
+    /// </summary>
+    public enum UpdateChannel
+    {
+        /// <summary>
+        /// Stable releases.
+        /// </summary>
+        [Description("Stable releases.")]
+        Stable,
+
+        /// <summary>
+        /// Pre-releases.
+        /// </summary>
+        [Description("Pre-releases.")]
+        PreRelease
+    }
+}
